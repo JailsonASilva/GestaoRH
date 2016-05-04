735392efde6920c095dbeb36c2ada5eed574f6ea
Jailson <Jailson@Jailson-PC>
2016-05-04 15:37:49 -0300

    Esse funcionou... graças a DEUS

--- src/test/java/br/pro/gestao/rh/util/HibernateUtilTeste.java
+++ src/test/java/br/pro/gestao/rh/util/HibernateUtilTeste.java
@@ -3,6 +3,8 @@ package br.pro.gestao.rh.util;
 import org.hibernate.Session;
 import org.junit.Test;
 
+import br.pro.gestao.rh.util.HibernateUtil;
+
 public class HibernateUtilTeste {
 	@Test
 	public void conectar(){
